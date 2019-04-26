6a9c6a80b2af7d5cb1c3770b7749107d0f3239d7
maxwellbenton <maxwellbenton@gmail.com>
2019-04-26 11:11:37 -0400

    sets starter code

---
+++ array_methods.rb
@@ -0,0 +1,11 @@
+def find_element_index(array, value_to_find)
+  # Add your solution here
+end
+
+def find_max_value(array)
+  # Add your solution here
+end
+
+def find_min_value(array)
+  # Add your solution here
+end
