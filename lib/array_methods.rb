bc9c9e541b638caad3252a130dda6eeaf4726767
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-02 04:39:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ def find_max_value(array)
   highest = array[0]
   array.length.times do |i|
     if(array[i] > highest)
-     highest =  array[i] 
+      highest =  array[i] 
     end
   end
   return highest  
