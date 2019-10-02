04ba07c2270aeb390d9a6733da4d2e6842b7d46f
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-02 03:57:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array
+  first = array[0]
+  array.length.times do |i|
+    if(first > array[i])
+end
+return
 end
 
 def find_min_value(array)
